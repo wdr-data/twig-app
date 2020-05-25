74f0c5ada91c343fa56256d525f5f2e32171d0f0
P3nny <p3nny@hacking.studio>
2020-05-25 17:08:02 +0200

    💄 Display tweet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React, { useCallback, useState } from "react";
-import "./App.css";
+import moment from "moment";
+import "moment/locale/de";
+
+import appStyles from "./App.module.css";
+import tweetStyles from "./Tweet.module.css";
+
+moment.locale("de");
+
+const baseUrl =
+  "https://zvbwmhjkp7.execute-api.eu-central-1.amazonaws.com/staging/status/";
+
+function Tweet({ tweet }) {
+  return (
+    <>
+      <h2>Vorschau: </h2>
+
+      <div className={tweetStyles.backgroundBlue}>
+        <div className={tweetStyles.backgroundWhite}>
+          <div className={tweetStyles.userContainer}>
+            <img src={tweet.user.profile_image_url_https} />
+            <div className={tweetStyles.userNameContainer}>
+              <span>{tweet.user.name}</span>
+              <span>@{tweet.user.screen_name}</span>
+            </div>
+          </div>
+          <p>{tweet.full_text}</p>
+          <span>
+            {moment(tweet.created_at).format("LT")} Uhr{" "}
+            {moment(tweet.created_at).format("LL")}
+          </span>
+          <img src={tweet.entities.media[0].media_url_https} />
+        </div>
+      </div>
+      <button>Download</button>
+    </>
+  );
+}
 
 function App() {
   const [tweet, setTweet] = useState();
+
   const newTweetCallback = useCallback(async () => {
-    const response = await fetch(
-      "https://zvbwmhjkp7.execute-api.eu-central-1.amazonaws.com/staging/status/1264239670973673474",
-    );
+    const tweetUrl = prompt("Tweet-URL eingeben");
+    if (!tweetUrl) {
+      return;
+    }
+    const tweetId = tweetUrl.match(/\d*$/)[0];
+    const response = await fetch(`${baseUrl}${tweetId}`);
     const tweet = await response.json();
-    console.log(tweet);
     setTweet(tweet);
+    console.log(tweet);
   }, []);
+
   return (
     <div className="App">
       <button onClick={newTweetCallback}>Neuer Tweet</button>
-      <div>Preview</div>
-      <button>Download</button>
+      {tweet && <Tweet tweet={tweet} />}
     </div>
   );
 }
