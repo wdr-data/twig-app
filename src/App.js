e8df37e02093cc7025b5966fa081aa1a27834216
Jannes Höke <mail@jhoeke.de>
2020-05-25 14:57:40 +0200

    🚧 Put tweet into state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [tweet, setTweet] = useState();
   const newTweetCallback = useCallback(async () => {
     const response = await fetch(
       "https://zvbwmhjkp7.execute-api.eu-central-1.amazonaws.com/staging/status/1264239670973673474",
     );
     const tweet = await response.json();
     console.log(tweet);
+    setTweet(tweet);
   }, []);
   return (
     <div className="App">
