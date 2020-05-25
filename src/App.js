861d5401036567af46dbb4c520372aa9c00a8a91
Jannes Höke <mail@jhoeke.de>
2020-05-25 14:57:24 +0200

    ✨ Use custom tweet API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { useCallback } from "react";
 import "./App.css";
 
-const twitterAuth = {
-  token_type: "bearer",
-  access_token: process.env.BEARER,
-};
-
 function App() {
   const newTweetCallback = useCallback(async () => {
-    const headers = new Headers({
-      Authorization: `Bearer ${twitterAuth.access_token}`,
-    });
     const response = await fetch(
-      "https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/statuses/show/210462857140252672.json",
-      {
-        mode: "cors",
-        headers,
-      }
+      "https://zvbwmhjkp7.execute-api.eu-central-1.amazonaws.com/staging/status/1264239670973673474",
     );
     const tweet = await response.json();
     console.log(tweet);
