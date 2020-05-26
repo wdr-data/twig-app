807498053cdd26b8b292282037e0e2659a40649c
Jannes Höke <mail@jhoeke.de>
2020-05-26 15:49:38 +0200

    ✨ Parse entities into JSX elements

--- src/App.js
+++ src/App.js
@@ -11,6 +11,61 @@ moment.locale("de");
 const baseUrl =
   "https://zvbwmhjkp7.execute-api.eu-central-1.amazonaws.com/staging/status/";
 
+const tokenizeTweet = (tweet) => {
+  const entities = tweet.entities;
+  const text = tweet.full_text;
+  const codepoints = Array.from(text);
+
+  const getSlice = (start, end) => codepoints.slice(start, end).join("");
+
+  const allEntities = [
+    ...entities.hashtags.map((entity) => ({ ...entity, kind: "hashtag" })),
+    ...entities.user_mentions.map((entity) => ({
+      ...entity,
+      kind: "user_mention",
+    })),
+    ...entities.urls.map((entity) => ({ ...entity, kind: "url" })),
+    ...entities.symbols.map((entity) => ({ ...entity, kind: "symbol" })),
+    ...codepoints
+      .map((char, i) =>
+        char === "\n" ? { kind: "break", indices: [i, i + 1] } : undefined
+      )
+      .filter((ent) => !!ent),
+    ...(entities.media || []).map((entity) => ({ ...entity, kind: "media" })),
+  ].sort((a, b) => a.indices[0] - b.indices[0]);
+
+  const nodes = [];
+  let currentIndex = 0;
+  for (const entity of allEntities) {
+    const [startIndex, endIndex] = entity.indices;
+
+    // Check if there is regular text between the last entity and this one
+    if (currentIndex !== startIndex) {
+      nodes.push(<span>{getSlice(currentIndex, startIndex)}</span>);
+    }
+
+    if (entity.kind === "url") {
+      nodes.push(
+        <span className={tweetStyles.highlightText}>{entity.display_url}</span>
+      );
+    } else if (entity.kind === "break") {
+      nodes.push(<br />);
+    } else if (entity.kind === "media") {
+      // Don't display media links
+    } else {
+      nodes.push(
+        <span className={tweetStyles.highlightText}>
+          {getSlice(startIndex, endIndex)}
+        </span>
+      );
+    }
+
+    currentIndex = endIndex;
+  }
+
+  return nodes;
+};
+
 function Tweet({ tweet }) {
   const tweetRef = useRef();
   const [image, setImage] = useState();
@@ -40,7 +95,7 @@ function Tweet({ tweet }) {
               </span>
             </div>
           </div>
-          <p className={tweetStyles.text}>{tweet.full_text}</p>
+          <p className={tweetStyles.text}>{tokenizeTweet(tweet)}</p>
           <span className={tweetStyles.timestamp}>
             {moment(tweet.created_at).format("LT")} Uhr{" "}
             {moment(tweet.created_at).format("LL")}
